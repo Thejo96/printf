d6abfb3d2b933cca7ebb5792411446570c832461
Thejo96 <thejopodcast2023@gmail.com>
2023-07-26 16:13:56 +0100

    Updated

--- main.h
+++ main.h
@@ -3,13 +3,18 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
+
+/* FLAGS */
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
+
+/* SIZES */
 #define S_LONG 2
 #define S_SHORT 1
 
@@ -24,7 +29,6 @@ struct fmt
 	char fmt;
 	int (*fn)(va_list, char[], int, int, int, int);
 };
-
 
 /**
  * typedef struct fmt fmt_t - Struct op
@@ -110,4 +114,3 @@ long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif /* MAIN_H */
-
