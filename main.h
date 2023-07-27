32d17deccc672de8bd8dbc98ebc3875574845bff
Thejo96 <thejopodcast2023@gmail.com>
2023-07-27 04:37:48 +0100

    Updated

--- main.h
+++ main.h
@@ -37,8 +37,7 @@ struct fmt
  * @fm_t: The function associated.
  */
 typedef struct fmt fmt_t;
-
-int _printf(const char *format, ...);
+int _printf("%.6s", "Best School !\n");
 int handle_print(const char *fmt, int *i,
 		va_list list, char buffer[], int flags, int width, int precision, int size);
 
