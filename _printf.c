0d269a800f5193843f7217d68e9531a01e614edb
Thejo96 <thejopodcast2023@gmail.com>
2023-07-28 18:54:42 +0100

    Updated

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf 
+ * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
@@ -53,7 +53,7 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents .
+ * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
@@ -64,3 +64,4 @@ void print_buffer(char buffer[], int *buff_ind)
 
 	*buff_ind = 0;
 }
+
